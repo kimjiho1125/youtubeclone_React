624896641f8af7d72e4d124a0269371c0458e6d5
JIHO <45422452+kimjiho1125@users.noreply.github.com>
2022-02-26 20:50:46 +0900

    video upload form 만들기

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import Dropzone from 'react-dropzone';
+
+const { TextArea } = Input;
+const { Title } = Typography;
 
 function VideoUploadPage() {
     return (
-        <div></div>
-    )
+      <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+        <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+          <Title level={2}>Upload Video</Title>
+        </div>
+
+        <Form onSubmit>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {/* Drop zoen */}
+            <Dropzone onDrop multiple maxSize>
+              {({ getRootProps, getInputProps }) => (
+                <div
+                  style={{
+                    width: "300px",
+                    height: "240px",
+                    border: "1px solid lightgray",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  {...getRootProps()}
+                >
+                  <input {...getInputProps()} />
+                  <Icon type="plus" style={{ fontSize: "3rem " }} />
+                </div>
+              )}
+            </Dropzone>
+            {/* Thumbnail */}
+            <div>
+              <img scr alt />
+            </div>
+          </div>
+
+          <br />
+          <br />
+          <label>Title</label>
+          <Input onChange value />
+
+          <br />
+          <br />
+          <label>Description</label>
+          <TextArea onChange value />
+
+          <br />
+          <br />
+          <select onChange>
+            <option key value></option>
+          </select>
+
+          <br />
+          <br />
+          <select onChange>
+            <option key value></option>
+          </select>
+
+          <br />
+          <br />
+          <Button type="primary" size="large" onclick>
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
 }
 
 export default VideoUploadPage
